289ed57070fbe295b85071d10dfc5a307f62467d
damien-dupre <damien.dupre.ac@gmail.com>
2019-01-24 11:39:58 +0000

    insertMutate

---
+++ R/insertMutate.R
@@ -0,0 +1,8 @@
+#' Insert dplyr::mutate().
+#'
+#' Call this function as an addin to insert \code{ dplyr::mutate() } at the cursor position.
+#'
+#' @export
+insertInAddin <- function() {
+    rstudioapi::insertText("dplyr::mutate()")
+}
